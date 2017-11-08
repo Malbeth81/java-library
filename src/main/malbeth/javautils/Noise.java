2796ab55b48463296c58104c6d95c0a8e989ddd5
Marc-Andre Lamothe <marc7141@gmail.com>
2017-11-07 21:40:47 -0500

    Improvements to the Noise & NoiseDemo classes

--- src/main/malbeth/javautils/Noise.java
+++ src/main/malbeth/javautils/Noise.java
@@ -1,190 +1,118 @@
 package malbeth.javautils;
 
+import java.util.Random;
+
 public class Noise {
 
-    public static double[] blend(double[] noise1, double[] noise2, double persistence) {
-        if (noise1 != null && noise2 != null && noise1.length > 0 && noise1.length == noise2.length) {
-            double[] result = new double[noise1.length];
-            for (int i = 0; i < noise1.length; i++)
-                result[i] = noise1[i] + (noise2[i] * persistence);
-            return result;
-        }
-
-        return null;
-    }
-
-    public static double[] cut(double[] noise, double cut) {
-        if (noise != null && noise.length > 0) {
-            double minValue = noise[0];
-            double maxValue = noise[0];
-            for (double aNoise : noise) {
-                if (aNoise < minValue)
-                    minValue = aNoise;
-                else if (aNoise > maxValue)
-                    maxValue = aNoise;
+    public static double[][] blend(double[][] noise1, double[][] noise2, double alpha) {
+        if (noise1 != null && noise2 != null && noise1.length > 0 && noise1.length == noise2.length && noise1[0].length == noise2[0].length) {
+            double[][] result = new double[noise1.length][noise1[0].length];
+            for (int i = 0; i < noise1.length; i++) {
+                if (noise1[i].length != noise1[0].length || noise2[i].length != noise1[i].length) {
+                    return null;
+                }
+                for (int j = 0; j < noise1[i].length; j++)
+                    result[i][j] = interpolate(noise1[i][j], noise2[i][j], alpha);
             }
-
-            return cut(noise, cut, minValue, maxValue);
-        }
-
-        return null;
-    }
-
-    public static double[] cut(double[] noise, double cut, double min, double max) {
-        if (noise != null && noise.length > 0) {
-            double[] result = new double[noise.length];
-
-            for (int i = 0; i < noise.length; i++)
-                result[i] = (noise[i] < (max - min) / (1 / cut) ? 1 : 0);
-
             return result;
         }
 
         return null;
     }
 
-    public static double[] normalize(double[] noise) {
-        if (noise != null && noise.length > 0) {
-            double[] result = new double[noise.length];
-
-            double minValue = noise[0];
-            double maxValue = noise[0];
-            for (double aNoise : noise) {
-                if (aNoise < minValue)
-                    minValue = aNoise;
-                else if (aNoise > maxValue)
-                    maxValue = aNoise;
-            }
-
-            for (int i = 0; i < noise.length; i++)
-                result[i] = (noise[i] - minValue) / (maxValue - minValue);
-
-            return result;
-        }
-
-        return null;
+    private  static double interpolate(double a, double b, double alpha) {
+        return a * (1 - alpha) + b * alpha;
     }
 
-    public static double[] perlinNoise(int width, int height, double scale) {
-        int[] p = new int[width * height];
-        double[] result = new double[width * height];
-        /*final int[] permutation = {151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
-                                   190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174,
-                                   20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230,
-                                   220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169,
-                                   200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147,
-                                   118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44,
-                                   154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104,
-                                   218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192,
-                                   214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24,
-                                   72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180};*/
+    public static double[][] perlinNoise(int width, int height, int octaveCount) {
+        return perlinNoise(whiteNoise(width, height), octaveCount);
+    }
 
-        for (int i = 0; i < p.length / 2; i++)
-            p[i] = p[i + p.length / 2] = (int) (Math.random() * p.length / 2);//permutation[i];
+    public static double[][] perlinNoise(double[][] noise, int octaveCount) {
+        if (noise == null || octaveCount <= 0)
+            return noise;
+
+        int width = noise.length;
+        int height = noise[0].length;
+
+        double[][] result = new double[width][height];
+        double amplitude = 1.0f;
+        double persistance = 0.5f;
+        double totalAmplitude = 0.0f;
+
+        double[][][] smoothNoise = new double[octaveCount][][];
+        for (int i = 0; i < octaveCount; i++)
+            smoothNoise[i] = smoothNoise(noise, i);
+
+        // Blend noise together
+        for (int octave = octaveCount - 1; octave >= 0; octave--) {
+            amplitude *= persistance;
+            totalAmplitude += amplitude;
+
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    result[i][j] += smoothNoise[octave][i][j] * amplitude;
+        }
+
+        // Normalisation
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                result[i][j] /= totalAmplitude;
+
+        return result;
+    }
+
+    public static double[][] smoothNoise(int width, int height, int octave) {
+        return smoothNoise(whiteNoise(width, height), octave);
+    }
+
+    public static double[][] smoothNoise(double[][] noise, int octave) {
+        if (noise == null || octave <= 0)
+            return noise;
+
+        int width = noise.length;
+        int height = noise[0].length;
+
+        double[][] result = new double[width][height];
+
+        int samplePeriod = 1 << octave; // calculates 2 ^ k
+        double sampleFrequency = 1.0f / samplePeriod;
 
         for (int i = 0; i < width; i++) {
+            // Calculate the horizontal sampling indices
+            int sample_i0 = (i / samplePeriod) * samplePeriod;
+            int sample_i1 = (sample_i0 + samplePeriod) % width; //wrap around
+            double horizontal_blend = (i - sample_i0) * sampleFrequency;
+
             for (int j = 0; j < height; j++) {
-                double x = i * scale / width;                                // FIND RELATIVE X,Y,Z
-                double y = j * scale / height;                                // OF POINT IN CUBE.
-                int X = (int) Math.floor(x) & 255;                  // FIND UNIT CUBE THAT
-                int Y = (int) Math.floor(y) & 255;                  // CONTAINS POINT.
-                int Z = 0;
-                x -= Math.floor(x);                                // FIND RELATIVE X,Y,Z
-                y -= Math.floor(y);                                // OF POINT IN CUBE.
-                double u = fade(x);                                // COMPUTE FADE CURVES
-                double v = fade(y);                                // FOR EACH OF X,Y,Z.
-                double w = fade(Z);
-                int A = p[X] + Y, AA = p[A] + Z, AB = p[A + 1] + Z,      // HASH COORDINATES OF
-                        B = p[X + 1] + Y, BA = p[B] + Z, BB = p[B + 1] + Z;      // THE 8 CUBE CORNERS,
+                // Calculate the vertical sampling indices
+                int sample_j0 = (j / samplePeriod) * samplePeriod;
+                int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
+                double vertical_blend = (j - sample_j0) * sampleFrequency;
 
-                result[j + i * width] = lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, Z),  // AND ADD
-                                        grad(p[BA], x - 1, y, Z)), // BLENDED
-                                lerp(u, grad(p[AB], x, y - 1, Z),  // RESULTS
-                                        grad(p[BB], x - 1, y - 1, Z))
-                        ),// FROM  8
-                        lerp(v, lerp(u, grad(p[AA + 1], x, y, Z - 1),  // CORNERS
-                                        grad(p[BA + 1], x - 1, y, Z - 1)), // OF CUBE
-                                lerp(u, grad(p[AB + 1], x, y - 1, Z - 1), grad(p[BB + 1], x - 1, y - 1, Z - 1))
-                        )
-                );
+                // Blend the top two corners
+                double top = interpolate(noise[sample_i0][sample_j0], noise[sample_i1][sample_j0], horizontal_blend);
+
+                // Blend the bottom two corners
+                double bottom = interpolate(noise[sample_i0][sample_j1], noise[sample_i1][sample_j1], horizontal_blend);
+
+                // Final blend
+                result[i][j] = interpolate(top, bottom, vertical_blend);
             }
         }
-        return result;
-    }
-
-    public static double[] smoothNoise(int width, int height, double zoom) {
-        if (zoom > 0) {
-            double[] noise = whiteNoise(width, height);
-            double[] result = new double[width * height];
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    double x = i / zoom;
-                    double y = j / zoom;
-
-                    // get fractional part of x and y
-                    double fractX = x - (int) x;
-                    double fractY = y - (int) y;
-
-                    // wrap around
-                    int x1 = ((int) x + width) % width;
-                    int y1 = ((int) y + height) % height;
-
-                    // neighbor values
-                    int x2 = (x1 + width - 1) % width;
-                    int y2 = (y1 + height - 1) % height;
-
-                    // smooth the noise with bilinear interpolation
-                    result[j + i * width] = fractX * fractY * noise[y1 + x1 * width]
-                            + fractX * (1 - fractY) * noise[y2 + x1 * width]
-                            + (1 - fractX) * fractY * noise[y1 + x2 * width]
-                            + (1 - fractX) * (1 - fractY) * noise[y2 + x2 * width];
-                }
-            }
-
-            return result;
-        }
-
-        return null;
-    }
-
-    public static double[] turbulence(int width, int height, double zoom) {
-        // http://lodev.org/cgtutor/randomnoise.html
-        double[] result = new double[width * height];
-        double initialZoom = zoom;
-
-        while (zoom >= 1) {
-            result = blend(result, smoothNoise(width, height, zoom), zoom);
-            zoom /= 2.0;
-        }
-
-        if (result != null) {
-            for (int i = 0; i < result.length; i++)
-                result[i] = (128.0 * result[i] / initialZoom);
-        }
 
         return result;
     }
 
-    public static double[] whiteNoise(int width, int height) {
-        double[] result = new double[width * height];
-        for (int i = 0; i < width * height; i++)
-            result[i] = Math.random();
+    public static double[][] whiteNoise(int width, int height) {
+        double[][] result = new double[width][height];
+
+        Random random = new Random();
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                result[i][j] = random.nextFloat();
+
         return result;
-    }
-
-    private static double fade(double t) {
-        return t * t * t * (t * (t * 6 - 15) + 10);
-    }
-
-    private static double lerp(double t, double a, double b) {
-        return a + t * (b - a);
-    }
-
-    private static double grad(int hash, double x, double y, double z) {
-        int h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
-        double u = h < 8 ? x : y,                 // INTO 12 GRADIENT DIRECTIONS.
-                v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-        return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
     }
 
 }
--- src/main/malbeth/javautils/NoiseDemo.java
+++ src/main/malbeth/javautils/NoiseDemo.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 public class NoiseDemo extends JFrame {
+    private double[][] noise;
+
     public NoiseDemo() throws ClassNotFoundException {
         initialize();
         setTitle("Noise tester");
@@ -20,86 +22,98 @@ public class NoiseDemo extends JFrame {
         panel.setLayout(null);
 
         final JLabel picture = new JLabel();
-        picture.setBounds(120, 20, 512, 512);
+        picture.setBounds(200, 20, 512, 512);
         picture.setVerticalAlignment(javax.swing.SwingConstants.TOP);
         panel.add(picture);
 
         final JTextField widthField = new JTextField();
-        widthField.setBounds(20, 20, 35, 24);
+        widthField.setBounds(20, 20, 75, 24);
         widthField.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         widthField.setText("512");
         panel.add(widthField);
 
         final JLabel sizeLabel = new JLabel();
-        sizeLabel.setBounds(56, 20, 9, 24);
+        sizeLabel.setBounds(96, 20, 9, 24);
         sizeLabel.setText("x");
         panel.add(sizeLabel);
 
         final JTextField heightField = new JTextField();
-        heightField.setBounds(65, 20, 35, 24);
+        heightField.setBounds(105, 20, 75, 24);
         heightField.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         heightField.setText("512");
         panel.add(heightField);
 
-        final JButton generateButton = new JButton("Map A");
-        generateButton.setBounds(20, 60, 80, 30);
-        generateButton.addActionListener(new ActionListener() {
+        final JLabel octaveLabel = new JLabel();
+        octaveLabel.setBounds(20, 50, 75, 24);
+        octaveLabel.setText("Octave:");
+        panel.add(octaveLabel);
+
+        final JTextField octaveField = new JTextField();
+        octaveField.setBounds(105, 50, 75, 24);
+        octaveField.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        octaveField.setText("5");
+        panel.add(octaveField);
+
+        final JButton mapButton1 = new JButton("White Noise");
+        mapButton1.setBounds(20, 90, 160, 30);
+        mapButton1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
                 int height = Math.max(Math.min(Integer.parseInt(heightField.getText()), 512), 0);
                 int width = Math.max(Math.min(Integer.parseInt(widthField.getText()), 512), 0);
-                double[] data = Noise.cut(Noise.smoothNoise(width, height, width / 10), 0.6);
-
-                for (int i = 0; i < data.length; i++)
-                    data[i] = 255 * data[i];
-
-                BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-                img.getRaster().setPixels(0, 0, width, height, data);
-
-                picture.setIcon(new ImageIcon(img));
+                noise = Noise.whiteNoise(width, height);
+                picture.setIcon(generateImage(noise));
             }
         });
-        panel.add(generateButton);
+        panel.add(mapButton1);
 
-        final JButton mapButton2 = new JButton("Map B");
-        mapButton2.setBounds(20, 100, 80, 30);
+        final JButton mapButton2 = new JButton("Smooth Noise");
+        mapButton2.setBounds(20, 130, 160, 30);
         mapButton2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                int height = Math.max(Math.min(Integer.parseInt(heightField.getText()), 512), 0);
-                int width = Math.max(Math.min(Integer.parseInt(widthField.getText()), 512), 0);
-                double[] data = Noise.cut(Noise.perlinNoise(width, height, 5), 0.2);
-
-                for (int i = 0; i < data.length; i++)
-                    data[i] = 255 * data[i];
-
-                BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-                img.getRaster().setPixels(0, 0, width, height, data);
-
-                picture.setIcon(new ImageIcon(img));
+                int octave = Math.max(Math.min(Integer.parseInt(octaveField.getText()),10), 0);
+                double[][] data = Noise.smoothNoise(noise, octave);
+                picture.setIcon(generateImage(data));
             }
         });
         panel.add(mapButton2);
 
-        final JButton mapButton3 = new JButton("Map C");
-        mapButton3.setBounds(20, 140, 80, 30);
+        final JButton mapButton3 = new JButton("Perlin Noise");
+        mapButton3.setBounds(20, 170, 160, 30);
         mapButton3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                int height = Math.max(Math.min(Integer.parseInt(heightField.getText()), 512), 0);
-                int width = Math.max(Math.min(Integer.parseInt(widthField.getText()), 512), 0);
-                double[] data = Noise.cut(Noise.blend(Noise.perlinNoise(width, height, 3), Noise.normalize(Noise.turbulence(width, height, width / 10)), 0.5), 0.4);
-
-                for (int i = 0; i < data.length; i++)
-                    data[i] = 255 * data[i];
-
-                BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-                img.getRaster().setPixels(0, 0, width, height, data);
-
-                picture.setIcon(new ImageIcon(img));
+                int octave = Math.max(Math.min(Integer.parseInt(octaveField.getText()),10), 0);
+                double[][] data = Noise.perlinNoise(noise, 1+octave);
+                picture.setIcon(generateImage(data));
             }
         });
         panel.add(mapButton3);
+
+        final JButton mapButton4 = new JButton("Smooth/Perlin");
+        mapButton4.setBounds(20, 210, 160, 30);
+        mapButton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int octave = Math.max(Math.min(Integer.parseInt(octaveField.getText()),10), 0);
+                double[][] data = Noise.blend(Noise.smoothNoise(noise, 1+octave), Noise.perlinNoise(noise, 1+octave), .5);
+                picture.setIcon(generateImage(data));
+            }
+        });
+        panel.add(mapButton4);
+
+        final JButton mapButton5 = new JButton("Smooth/Smooth");
+        mapButton5.setBounds(20, 250, 160, 30);
+        mapButton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int octave = Math.max(Math.min(Integer.parseInt(octaveField.getText()),10), 0);
+                double[][] data = Noise.blend(Noise.smoothNoise(noise, 1+octave), Noise.smoothNoise(noise, octave), .5);
+                picture.setIcon(generateImage(data));
+            }
+        });
+        panel.add(mapButton5);
     }
 
     public static void main(String[] args) {
@@ -114,6 +128,21 @@ public class NoiseDemo extends JFrame {
                 }
             }
         });
+    }
+
+    private ImageIcon generateImage(double[][] data) {
+        int width = data.length;
+        int height = data[0].length;
+
+        double[] imgData = new double[width * height];
+        for (int i = 0; i < data.length; i++)
+            for (int j = 0; j < data[i].length; j++)
+                imgData[j + i * width] = 255 * data[i][j];
+
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        img.getRaster().setPixels(0, 0, width, height, imgData);
+
+        return new ImageIcon(img);
     }
 
 }
